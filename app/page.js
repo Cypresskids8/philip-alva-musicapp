9ab3c9f6dc698e2b02f4ec5b91c4ccc8bd9f56f0
Cypresskids8 <134014487+Cypresskids8@users.noreply.github.com>
2024-04-29 18:53:09 -0500

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import styles from './style.css'
-import Navbar from "./navbar"
-import Footer from "./footer"
-import Body from "./body"
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Body from "./Body"
 
 export default function Home() {
   return (
